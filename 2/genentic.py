26a1acc12e01900c240e9eb895e86668e0ea6482
alkaitagi <alkaitagi@outlook.com>
2020-04-23 09:44:25 +0300

    Refactor

--- 2/genentic.py
+++ 2/genentic.py
@@ -1,46 +1,63 @@
+import types
 import random
 from PIL import Image, ImageDraw, ImageFont
 
 
-def fit(img):
-    pixels = img.load()
-    fit = 0
+def randcolor():
+    return tuple(random.randint(0, 255) for _ in range(3))
 
-    for i in range(W):
-        for j in range(H):
-            gPixel = pixels[i, j]
-            sPixel = sPixels[i, j]
+
+def getfit(individual):
+    fit = 0
+    for i in range(field):
+        for j in range(field):
+            iPixel = individual.pixels[i, j]
+            sPixel = source.pixels[i, j]
             for c in range(3):
-                fit += (gPixel[c] - sPixel[c]) ** 2
+                fit += (iPixel[c] - sPixel[c]) ** 2
 
     return fit
 
 
-def draw(img):
-    color = tuple(random.randint(0, 255) for _ in range(3))
-    string = random.choice(strings)
-    point = (random.randint(-fontsize, W),
-             random.randint(-fontsize, H))
-
-    draw = ImageDraw.Draw(img)
-    draw.text(point, string, font=font, fill=color)
+def mutate(individual):
+    
 
 
-strings = list("أبجدهوزحطيكلمنسعفصقرشتثخذ")
-fontsize = 50
-bounds = fontsize // 2
-# strings = list("qwertyuiopasdfghjklzxcvbnm")
+def populate(N):
+    population = []
+    for _ in range(N):
+        individual = types.SimpleNamespace()
+        individual.image = Image.new('RGB', source.size, (255, 255, 255))
+        individual.pixels = individual.image.load()
+        individual.draw = ImageDraw.Draw(individual.image)
+        individual.genes = []
+        for area in areas:
+            color = randcolor()
+            individual.genes.append(color)
+            individual.draw.ellipse(area, color)
 
-source = Image.open("image.png")
-font = ImageFont.truetype('font.ttf', fontsize)
+        population.append(individual)
 
-sPixels = source.load()
-W, H = source.size
+    return population
 
-population = [Image.new('RGB', source.size, (255, 255, 255))
-              for _ in range(10)]
 
-for _ in range(5000):
+field = 512
+diameter = 4
+areas = []
+arearange = range(len(areas))
+
+for i in range(field // diameter):
+    for j in range(field // diameter):
+        p = (diameter * i, diameter * j)
+        areas.append((p[0], p[1], p[0] + diameter, p[1] + diameter))
+
+source = types.SimpleNamespace()
+source.image = Image.open("image.png")
+source.pixels = source.image.load()
+
+population = populate(10)
+
+for _ in range(100000):
     draw(population[0])
     # for item in population:
     #     draw(item)
