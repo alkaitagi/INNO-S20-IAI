853b5d10851d14e677a6bfe1635fcd54a161f9e9
alkaitagi <alkaitagi@outlook.com>
2020-04-24 15:41:57 +0300

    At least it still runs

--- 2/genetic.py
+++ 2/genetic.py
@@ -5,29 +5,29 @@ import numpy as np
 
 
 def randcolor():
-    return tuple(random.randint(0, 255) for _ in range(3))
+    return np.round(255 * np.random.rand(1, 3))
 
 
-def draw(individual, i, color):
-    radius = diameter // 2
-    individual.image = cv2.circle(
-        individual.image, (areas[i][0] + radius, areas[i][1] + radius), radius, color, -1)
-    individual.genes[i] = color
+def draw(ind, i, color):
+    ind.gns[i] = color
+
+    # radius = diameter // 2
+    # individual.image = cv2.circle(
+    #     individual.image, (areas[i][0] + radius, areas[i][1] + radius), radius, color, -1)
+
+    x1, y1, x2, y2 = areas[i]
+    ind.img[x1:x2, y1:y2] = color
 
 
-def fit(individual, rect=None):
+def fit(ind, rect=None):
     if rect == None:
-        rect = (0, 0, field, field)
+        rect = (0, 0, size, size)
 
-    fit = 0
-    for i in range(rect[0], rect[2]):
-        for j in range(rect[1], rect[3]):
-            iPixel = individual.image[i, j]
-            sPixel = source[i, j]
-            for c in range(3):
-                fit += (iPixel[c] - sPixel[c]) ** 2
+    x1, y1, x2, y2 = rect
+    A = ind.img[x1:x2, y1:y2]
+    B = src[x1:x2, y1:y2]
 
-    return fit
+    return np.sum(np.square(np.subtract(A, B)))
 
 
 def crossover(A, B):
@@ -35,69 +35,82 @@ def crossover(A, B):
     if pivot < genelen // 2:
         A, B = B, A
 
-    individual = types.SimpleNamespace()
-    individual.image = A.image.copy()
-    individual.genes = A.genes[:]
+    ind = types.SimpleNamespace()
+    ind.img = A.img.copy()
+    ind.gns = A.gns[:]
     for i in range(genelen):
         if i > pivot:
-            draw(individual, i, B.genes[i])
+            draw(ind, i, B.gns[i])
 
-    individual.fit = fit(individual)
-    return individual
+    ind.fit = fit(ind)
+    return ind
 
 
-def mutate(individual):
+def mutate(ind):
     for i in random.choices(range(genelen), k=mutation):
-        individual.fit -= fit(individual, areas[i])
-        draw(individual, i, randcolor())
-        individual.fit += fit(individual, areas[i])
+        ind.fit -= fit(ind, areas[i])
+
+        color = np.random.uniform(-1, 1, 3)
+        color = np.multiply(color, randcolor())
+        color = np.add(ind.gns[i], color)
+        color = np.clip(color, 0, 255)
+        draw(ind, i, color)
+
+        ind.fit += fit(ind, areas[i])
 
 
 def populate(N):
-    population = []
+    pop = []
     for _ in range(N):
-        individual = types.SimpleNamespace()
-        individual.image = np.zeros((field, field, 3))
-        individual.genes = [None] * genelen
+        ind = types.SimpleNamespace()
+        ind.img = np.zeros((size, size, 3))
+        ind.gns = [None] * genelen
         for i in range(genelen):
-            draw(individual, i, randcolor())
+            draw(ind, i, randcolor())
 
-        individual.fit = fit(individual)
-        population.append(individual)
+        ind.fit = fit(ind)
+        pop.append(ind)
 
-    return population
+    return pop
 
 
-source = cv2.imread("test.png")
-field = source.shape[0]
-diameter = 4
+src = cv2.imread("image.png")
+size = src.shape[0]
+diameter = 32
 areas = []
 popcount = 10
-for i in range(field // diameter):
-    for j in range(field // diameter):
+for i in range(size // diameter):
+    for j in range(size // diameter):
         p = (diameter * i, diameter * j)
         areas.append((p[0], p[1], p[0] + diameter, p[1] + diameter))
 
 genelen = len(areas)
-mutation = int(0.02 * genelen)
-crosspool = int(0.25 * popcount)
+mutation = int(0.01 * genelen)
+crosspool = int(0.5 * popcount)
 pairs = list(range(popcount))
 
 
-population = populate(popcount)
+pop = populate(popcount)
+gnr = 0
 while True:
+    gnr += 1
+
     for _ in range(crosspool):
         A = random.choice(pairs)
         pairs.remove(A)
         B = random.choice(pairs)
         pairs.append(A)
 
-        child = crossover(population[A], population[B])
-        mutate(child)
-        population.append(child)
+        new = crossover(pop[A], pop[B])
+        mutate(new)
+        pop.append(new)
 
-    population.sort(key=lambda x: x.fit)
-    del population[popcount:]
+    pop.sort(key=lambda x: x.fit)
+    del pop[popcount:]
 
-    cv2.imwrite("result.png", population[0].image)
-    print(population[0].fit)
+
+    if gnr % 10 == 0:
+        print(gnr, ": ", pop[0].fit)
+        cv2.imwrite("result.png", pop[0].img)
+        # cv2.imshow("result", pop[0].img)
+        # cv2.waitKey(100)
