f7e22becd0bab6c9f44de5dfe69aebb6f43bfd7c
alkaitagi <alkaitagi@outlook.com>
2020-04-25 01:47:35 +0300

    Fixes

--- 2/genetic.py
+++ 2/genetic.py
@@ -1,56 +1,53 @@
 import types
-import random
 import cv2
 import numpy as np
 
 
 def rclr():
-    return tuple(random.randint(0, 255) for _ in range(3))
+    return tuple(np.random.randint(0, 255, 3))
 
 
 def rpnt():
-    return (random.randint(0, W), random.randint(0, H))
+    return tuple(np.random.randint(0, S, 2))
 
 
-def fit(img, x1, y1, x2, y2):
-    return np.sum(np.square(np.subtract(img[x1:x2, y1:y2], src[x1:x2, y1:y2])))
+def fit(img, x, y, u, v):
+    return np.sum(np.square(np.subtract(img[x:u, y:v], src[x:u, y:v])))
 
 
-def mutate(res):
-    ind = types.SimpleNamespace()
-    ind.img = res.img.copy()
-    ind.fit = res.fit
+def mutate(ind):
+    x, y, = rpnt()
+    u, v = (x, y) + np.random.randint(5, 15, 2)
 
-    pnt = rpnt()
-    rds = random.randint(10, 20)
-    x1, x2, y1, y2 = pnt[0] - rds, pnt[0] + rds, pnt[1] - rds, pnt[1] + rds
-
-    ind.fit -= fit(ind.img, x1, y1, x2, y2)
-    ind.img = cv2.circle(ind.img, pnt, rds, rclr(), -1)
-    ind.fit += fit(ind.img, x1, y1, x2, y2)
+    ind.fit -= fit(ind.img, x, y, u, v)
+    ind.img[x:u, y:v] = rclr()
+    ind.fit += fit(ind.img, x, y, u, v)
 
     return ind
 
 
-src = cv2.imread("image.png")
-W, H = src.shape[:2]
-N = 5
+src = cv2.imread("mona.png")
+S = src.shape[0]
+N = 50
 
 res = types.SimpleNamespace()
-res.img = np.ones((W, H, 3))
-res.fit = fit(res.img, 0, 0, W, H)
+res.img = np.ones((S, S, 3))
+res.fit = fit(res.img, 0, 0, S, S)
 
 gnr = 0
 while True:
     bst = res
     for _ in range(N):
-        ind = mutate(res)
+        ind = types.SimpleNamespace()
+        ind.img = res.img.copy()
+        ind.fit = res.fit
+        mutate(ind)
         if ind.fit < bst.fit:
             bst = ind
 
     res = bst
-
     gnr += 1
-    if gnr % 10 == 0:
+
+    if not gnr % 10:
         print(gnr, ": ", res.fit)
         cv2.imwrite("result.png", res.img)
