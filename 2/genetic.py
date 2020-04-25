e40c0e300c0e1089b39a8354851bb639d8f9c1e6
alkaitagi <alkaitagi@outlook.com>
2020-04-25 10:43:12 +0300

    Somethign something lines

--- 2/genetic.py
+++ 2/genetic.py
@@ -14,47 +14,54 @@ class Ind:
 
 
 def rclr():
-    return [np.random.randint(0, 255) for _ in range(3)]
+    # return [np.random.randint(0, 255) for _ in range(3)]
+    return (0, 0, 0) if np.random.rand() < .5 else (255, 255, 255)
 
 
 def rrot():
     return np.random.uniform(-np.pi, np.pi)
 
 
+def line(x, y, rot, rad):
+    a, b = int(rad * np.cos(rot)), int(rad * np.sin(rot))
+    return (x + a, y + b), (x - a, y - b)
+
+
 def mut(ind):
     x, y = C * np.random.randint(0, G, 2)
     u, v = x + C, y + C
 
-    # rot = rrot()
-    # x, y = int(C * np.cos(rot)), int(C * np.sin(rot))
-
-    # p1 = (pnt[0] + x, pnt[1] + y)
-    # p2 = (pnt[0] - x, pnt[1] - y)
-
     ind.fit -= fit(ind.img, x, y, u, v)
-    cv2.rectangle(ind.img, (x, y), (u, v), rclr(), -1)
+
+    l = np.random
+    p1, p2 = line(x + Ch, y + Ch, 0, C + 1)
+    cv2.line(ind.img, p1, p2, (255, 255, 255), 1)
+    p1, p2 = line(x + Ch, y + Ch, 0, C)
+    cv2.line(ind.img, p1, p2, rclr(), 1)
+
     ind.fit += fit(ind.img, x, y, u, v)
 
     return ind
 
 
 def fit(img, x, y, u, v):
-    return np.sum(np.absolute(np.subtract(img[x:u, y:v], src[x:u, y:v])))
+    return np.sum(np.absolute(np.subtract(img[y:v, x:u], src[y:v, x:u])))
 
 
-src = cv2.imread(r"C:\Users\alkaitagi\Projects\INNO-S20-IAI\2\mona.png")
+src = cv2.imread("mona.png")
 
 # image size
 S = src.shape[0]
 # cell size
 C = 2
+Ch = C // 2
 # grid size
 G = S // C
 # population
 N = 50
 
 res = Ind()
-res.img = np.ones((S, S, 3))
+res.img = 255 * np.ones((S, S, 3))
 res.fit = fit(res.img, 0, 0, S, S)
 
 
@@ -69,6 +76,6 @@ while True:
     res = bst
     gnr += 1
 
-    if not gnr % 10:
+    if not gnr % 100:
         print(gnr, ": ", res.fit)
         cv2.imwrite("result.png", res.img)
