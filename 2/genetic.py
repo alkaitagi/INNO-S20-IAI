9f018e4b1c787eb4be6b9f2af61c2220b2e6138b
alkaitagi <alkaitagi@outlook.com>
2020-04-25 17:10:56 +0300

    Clean up

--- 2/genetic.py
+++ 2/genetic.py
@@ -13,18 +13,6 @@ class Ind:
         return ind
 
 
-def rclr():
-    # c = np.random.randint(0, 256)
-    # return (c, c, c)
-    return [np.random.randint(0, 255) for _ in range(3)]
-
-
-def rrot():
-    return (-1 if np.random.rand() < .5 else 1) * .25 * np.pi
-    # return 0 if np.random.rand() < .5 else .5 * np.pi
-    # return np.random.uniform(-.5, .5) * np.pi
-
-
 def line(x, y, rot, rad):
     a, b = int(rad * np.cos(rot)), int(rad * np.sin(rot))
     return (x + a, y + b), (x - a, y - b)
@@ -34,15 +22,14 @@ def mut(ind):
     x = np.random.randint(0, H)
     y = np.random.randint(0, W)
 
-    l = np.random.randint(L[0], L[1])
-    r = rrot()
+    lng = T * np.random.randint(L[0], L[1])
+    rot = np.random.choice([-.25, 0, .25, .5])
+    col = [np.random.randint(0, 255) for _ in range(3)]
 
-    ind.fit -= fit(ind.img, x - l, y - l, x + l, y + l)
-
-    p1, p2 = line(x, y, r, l)
-    cv2.line(ind.img, p1, p2, rclr(), 1)
-
-    ind.fit += fit(ind.img, x - l, y-l, x + l, y + l)
+    ind.fit -= fit(ind.img, x - lng, y - lng, x + lng, y + lng)
+    p1, p2 = line(x, y, rot, lng)
+    cv2.line(ind.img, p1, p2, col, T)
+    ind.fit += fit(ind.img, x - lng, y - lng, x + lng, y + lng)
 
     return ind
 
@@ -51,14 +38,14 @@ def fit(img, x, y, u, v):
     return np.sum(np.absolute(np.subtract(img[y:v, x:u], src[y:v, x:u])))
 
 
-src = cv2.imread(r"C:\Users\alkaitagi\Projects\INNO-S20-IAI\2\imam.png")
+src = cv2.imread("source.png")
 
 # radius range
-L = (5, 20)
+L = (4, 12)
 # image size
 W, H = src.shape[:2]
-# gap
-G = 2
+# thichkness
+T = 2
 # population
 N = 50
 
