19f73fb00829d6566fc3200cbb57241ab47160b6
alkaitagi <alkaitagi@outlook.com>
2020-04-25 21:12:07 +0300

    Fixes???

--- 2/genetic.py
+++ 2/genetic.py
@@ -13,53 +13,57 @@ class Ind:
         return ind
 
 
-def line(x, y, rot, lng):
+def line(y, x, rot, lng):
     if not rot:
-        return (x - lng, y), (x + lng, y)
+        return y, x - lng, y, x + lng
     elif rot == .5:
-        return (x, y - lng), (x, y + lng)
+        return y - lng, x,  y + lng, x
     elif rot == .25:
-        return (x - lng, y - lng), (x + lng, y + lng)
+        return y - lng, x - lng, y + lng, x + lng
     else:
-        return (x - lng, y + lng), (x + lng, y - lng)
-        
+        return y + lng, x - lng, y - lng, x + lng
 
 
 def mut(ind):
-    x = T // 2 + T * np.random.randint(0, H // T)
-    y = T // 2 + T * np.random.randint(0, W // T)
+    y = T // 2 + T * np.random.randint(0, H // T)
+    x = T // 2 + T * np.random.randint(0, W // T)
 
     lng = T * np.random.randint(L[0], L[1])
-    rot = np.random.choice([-.25, 0, .25, .5])
+    # rot = np.random.choice([-.25, 0, .25, .5])
+    rot = np.random.choice([0, .5])
     col = [np.random.randint(0, 255) for _ in range(3)]
-    pt1, pt2 = line(x, y, rot, lng)
 
-    ind.fit -= fit(ind.img, x - lng, y - lng, x + lng, y + lng)
-    cv2.line(ind.img, pt1, pt2, col, T)
-    ind.fit += fit(ind.img, x - lng, y - lng, x + lng, y + lng)
+    b, a, d, c = line(y, x, rot, lng)
+
+    ind.fit -= fit(ind.img, b, a, d, c)
+    cv2.line(ind.img, (a, b), (c, d), col, T)
+    ind.fit += fit(ind.img, b, a, d, c)
 
     return ind
 
 
-def fit(img, x, y, u, v):
-    return np.sum(np.absolute(np.subtract(img[y:v, x:u], src[y:v, x:u])))
+def fit(img, b, a, d, c):
+    b, d = np.sort(np.clip([b, d], 0, H))
+    a, c = np.sort(np.clip([a, c], 0, W))
+    print(b, a, d, c)
+    return np.sum(np.absolute(np.subtract(img[b:d, a:c], src[b:d, a:c])))
 
 
-src = cv2.imread("source.png")
-
-# radius range
-L = (4, 12)
+# source image
+src = cv2.imread("mona.png")
 # image size
-W, H = src.shape[:2]
-# thickness
-T = 2
-# population
-N = 1
+H, W = src.shape[:2]
+# stoke radius range (ratio to thickness)
+L = (2, 4)
+# stroke thickness
+T = 1
+# variants population
+N = 5
 
 
 res = Ind()
-res.img = 255 * np.ones((W, H, 3))
-res.fit = fit(res.img, 0, 0, W, H)
+res.img = 255 * np.ones((H, W, 3))
+res.fit = fit(res.img, 0, 0, H, W)
 
 
 gnr = 0
