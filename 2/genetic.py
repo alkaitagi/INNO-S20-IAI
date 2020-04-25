cd391def29ae8d19c13fe0290627d7dd0b6e7b33
alkaitagi <alkaitagi@outlook.com>
2020-04-25 09:20:33 +0300

    Sueta

--- 2/genetic.py
+++ 2/genetic.py
@@ -1,73 +1,42 @@
 import cv2
 import numpy as np
-from scipy.spatial import Delaunay
 
 
 class Ind:
     img = None
-    trg = []
     fit = 0
-
-    def __init__(self, img=None, fit=0, trg=None):
-        self.img = img
-        self.fit = fit
-        self.trg = trg
+    last = None
 
     def copy(self):
-        return Ind(self.img.copy(), self.fit, self.trg[:])
-
-
-def bnd(pnts):
-    x = min(pnts, key=lambda x: x[0])[0]
-    y = min(pnts, key=lambda x: x[1])[1]
-    u = max(pnts, key=lambda x: x[0])[0]
-    v = max(pnts, key=lambda x: x[1])[1]
-    return x, y, u, v
-
-
-def chk(pnt, trg):
-    A = .5 * (-trg[1][1] * trg[2][0] + trg[0][1] * (-trg[1][0] + trg[2][0]) +
-              trg[0][0] * (trg[1][1] - trg[2][1]) + trg[1][0] * trg[2][1])
-    sign = -1 if A < 0 else 1
-    s = (trg[0][1] * trg[2][0] - trg[0][0] * trg[2][1] + (trg[2][1] - trg[0][1])
-         * pnt[0] + (trg[0][0] - trg[2][0]) * pnt[1]) * sign
-    t = (trg[0][0] * trg[1][1] - trg[0][1] * trg[1][0] + (trg[0][1] - trg[1][1])
-         * pnt[0] + (trg[1][0] - trg[0][0]) * pnt[1]) * sign
-
-    return s > 0 and t > 0 and (s + t) < 2 * A * sign
-
-
-def ins(pnt, trg):
-    t1 = np.array([trg[0], trg[1], pnt])
-    t2 = np.array([trg[0], trg[2], pnt])
-    t3 = np.array([trg[1], trg[2], pnt])
-    return [t1, t2, t3]
+        ind = Ind()
+        ind.img = self.img.copy()
+        ind.fit = self.fit
+        return ind
 
 
 def rclr():
     return [np.random.randint(0, 255) for _ in range(3)]
 
 
-def rpnt():
-    return np.random.randint(0, S, 2)
+def rrot():
+    return np.random.uniform(-np.pi, np.pi)
 
 
 def mut(ind):
-    pnt = rpnt()
-    for i, t in enumerate(ind.trg):
-        if chk(pnt, t):
-            sht = ins(pnt, t)
-            x, y, u, v = bnd(t)
+    x, y = C * np.random.randint(0, G, 2)
+    u, v = (x + C, y+C)
 
-            ind.fit -= fit(ind.img, x, y, u, v)
-            for s in sht:
-                ind.img = cv2.drawContours(ind.img, [s], 0, rclr(), -1)
-            ind.fit += fit(ind.img, x, y, u, v)
+    ind.last = [x, y]
 
-            del ind.trg[i]
-            ind.trg += sht
+    # rot = rrot()
+    # x, y = int(C * np.cos(rot)), int(C * np.sin(rot))
 
-            break
+    # p1 = (pnt[0] + x, pnt[1] + y)
+    # p2 = (pnt[0] - x, pnt[1] - y)
+
+    ind.fit -= fit(ind.img, x, y, u, v)
+    ind.img[x:u, y:v] = rclr()
+    ind.fit += fit(ind.img, x, y, u, v)
 
     return ind
 
@@ -80,26 +49,16 @@ src = cv2.imread("mona.png")
 
 # image size
 S = src.shape[0]
+# cell size
+C = 2
+# grid size
+G = S // C
 # population
 N = 50
 
-pnts = []
-for _ in range(50):
-    pnts.append(rpnt())
-pnts = np.array(pnts)
-tri = Delaunay(pnts)
-
-img = np.ones((S, S, 3))
-for t in pnts[tri.simplices]:
-    img = cv2.drawContours(img, [t], 0, rclr(), -1)
-cv2.imwrite("result.png", img)
-print("done")
-input()
-
-res = Ind(np.ones((S, S, 3)))
+res = Ind()
+res.img = np.ones((S, S, 3))
 res.fit = fit(res.img, 0, 0, S, S)
-res.trg = [np.array([[0, 0], [S, 0], [0, S]]),
-           np.array([[0, S], [S, 0], [S, S]])]
 
 
 gnr = 0
@@ -114,5 +73,5 @@ while True:
     gnr += 1
 
     if not gnr % 10:
-        print(gnr, ": ", res.fit)
+        print(gnr, ": ", res.fit, res.last)
         cv2.imwrite("result.png", res.img)
