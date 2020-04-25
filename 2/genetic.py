37ad1c01a4c37a843085aa05d746a89538cf198c
alkaitagi <alkaitagi@outlook.com>
2020-04-25 04:26:41 +0300

    Add triangle bound, check, split

--- 2/genetic.py
+++ 2/genetic.py
@@ -4,14 +4,43 @@ import numpy as np
 
 class Ind:
     img = None
+    trg = []
     fit = 0
 
-    def __init__(self, img=None, fit=0):
+    def __init__(self, img=None, fit=0, trg=None):
         self.img = img
         self.fit = fit
+        self.trg = trg
 
     def copy(self):
-        return Ind(self.img.copy(), self.fit)
+        return Ind(self.img.copy(), self.fit, self.trg[:])
+
+
+def bnd(pnts):
+    x = min(pnts, key=lambda x: x[0])
+    y = min(pnts, key=lambda x: x[1])
+    u = max(pnts, key=lambda x: x[0])
+    v = max(pnts, key=lambda x: x[1])
+    return x, y, u, v
+
+
+def chk(pnt, trg):
+    A = .5 * (-trg[1][1] * trg[2][0] + trg[0][1] * (-trg[1][0] + trg[2][0]) +
+              trg[0][0] * (trg[1][1] - trg[2][1]) + trg[1][0] * trg[2][1])
+    sign = -1 if A < 0 else 1
+    s = (trg[0][1] * trg[2][0] - trg[0][0] * trg[2][1] + (trg[2][1] - trg[0][1])
+         * pnt[0] + (trg[0][0] - trg[2][0]) * pnt[1]) * sign
+    t = (trg[0][0] * trg[1][1] - trg[0][1] * trg[1][0] + (trg[0][1] - trg[1][1])
+         * pnt[0] + (trg[1][0] - trg[0][0]) * pnt[1]) * sign
+
+    return s > 0 and t > 0 and (s + t) < 2 * A * sign
+
+
+def ins(pnt, trg):
+    t1 = [trg[0], trg[1], pnt]
+    t2 = [trg[0], trg[2], pnt]
+    t3 = [trg[1], trg[2], pnt]
+    return [t1, t1, t3]
 
 
 def rclr():
@@ -52,6 +81,7 @@ A, B = 5, 15
 
 res = Ind(np.ones((S, S, 3)))
 res.fit = fit(res.img, 0, 0, S, S)
+res.trg = [[[0, 0], [S, 0], [0, S]], [0, S], [S, 0], [S, S]]
 
 gnr = 0
 while True:
