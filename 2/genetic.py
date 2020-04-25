c39b92d263e0b65c2c746ac3c7e8e96fb5c3d364
alkaitagi <alkaitagi@outlook.com>
2020-04-25 05:04:46 +0300

    At least it runs

--- 2/genetic.py
+++ 2/genetic.py
@@ -17,10 +17,10 @@ class Ind:
 
 
 def bnd(pnts):
-    x = min(pnts, key=lambda x: x[0])
-    y = min(pnts, key=lambda x: x[1])
-    u = max(pnts, key=lambda x: x[0])
-    v = max(pnts, key=lambda x: x[1])
+    x = min(pnts, key=lambda x: x[0])[0]
+    y = min(pnts, key=lambda x: x[1])[1]
+    u = max(pnts, key=lambda x: x[0])[0]
+    v = max(pnts, key=lambda x: x[1])[1]
     return x, y, u, v
 
 
@@ -37,31 +37,36 @@ def chk(pnt, trg):
 
 
 def ins(pnt, trg):
-    t1 = [trg[0], trg[1], pnt]
-    t2 = [trg[0], trg[2], pnt]
-    t3 = [trg[1], trg[2], pnt]
-    return [t1, t1, t3]
+    t1 = np.array([trg[0], trg[1], pnt])
+    t2 = np.array([trg[0], trg[2], pnt])
+    t3 = np.array([trg[1], trg[2], pnt])
+    return [t1, t2, t3]
 
 
 def rclr():
-    return np.random.randint(0, 255, 3)
+    return [np.random.randint(0, 255) for _ in range(3)]
 
 
 def rpnt():
     return np.random.randint(0, S, 2)
 
 
-def rgen():
-    return np.random.randint(A, B, 2)
-
-
 def mut(ind):
-    x, y, = rpnt()
-    u, v = [x, y] + rgen()
+    pnt = rpnt()
+    for i, t in enumerate(ind.trg):
+        if chk(pnt, t):
+            sht = ins(pnt, t)
+            x, y, u, v = bnd(t)
 
-    ind.fit -= fit(ind.img, x, y, u, v)
-    ind.img[x:u, y:v] = rclr()
-    ind.fit += fit(ind.img, x, y, u, v)
+            ind.fit -= fit(ind.img, x, y, u, v)
+            for s in sht:
+                ind.img = cv2.drawContours(ind.img, [s], 0, rclr(), -1)
+            ind.fit += fit(ind.img, x, y, u, v)
+
+            del ind.trg[i]
+            ind.trg += sht
+
+            break
 
     return ind
 
@@ -70,18 +75,18 @@ def fit(img, x, y, u, v):
     return np.sum(np.absolute(np.subtract(img[x:u, y:v], src[x:u, y:v])))
 
 
-src = cv2.imread("source.png")
+src = cv2.imread("mona.png")
 
 # image size
 S = src.shape[0]
 # population
 N = 50
-# rect side range
-A, B = 5, 15
 
 res = Ind(np.ones((S, S, 3)))
 res.fit = fit(res.img, 0, 0, S, S)
-res.trg = [[[0, 0], [S, 0], [0, S]], [0, S], [S, 0], [S, S]]
+res.trg = [np.array([[0, 0], [S, 0], [0, S]]),
+           np.array([[0, S], [S, 0], [S, S]])]
+
 
 gnr = 0
 while True:
